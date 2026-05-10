c56961b9ac1347c15ac7d39a2fd653f87406db65
personal <musharrafansari688@gmail.com>
2026-05-10 18:38:42 +0530

    Docker added

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get("/demo", (req, res) => {
+    res.json({
+        success: true,
+        message: 'This is a demo route'
+    });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
