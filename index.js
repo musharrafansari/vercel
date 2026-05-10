8a18de441a8b99e0e520d94db152bae0dccad44a
personal <musharrafansari688@gmail.com>
2026-05-10 16:44:41 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        success: true,
+        message: 'Render Deployment Successful'
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
