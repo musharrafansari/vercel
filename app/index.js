187774c19b8085258d0866c1738426203beff4e1
musharrafansari <105534999+musharrafansari@users.noreply.github.com>
2026-05-10 16:15:46 +0530

    Refactor index.js by removing unnecessary code
    Removed middleware, routes, error handling, and server start code.

--- app/index.js
+++ app/index.js
@@ -1,29 +1,12 @@
 const express = require('express');
+
 const app = express();
 
-// Middleware
-app.use(express.json());
-app.use(express.static('public'));
-
-// Routes
-app.get('/demo', (req, res) => {
-  res.send('Hello, World!');
-});
-
 app.get('/', (req, res) => {
-  res.send('Hiiiiiiiiiiiiiii');
+    return res.status(200).json({
+        success: true,
+        message: 'Vercel Deployment Successful'
+    });
 });
 
-
-
-// Error handling
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
-});
-
-// Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
